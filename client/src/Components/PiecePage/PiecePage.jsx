c04cbe5dfa7c0ecc0d45b7a5a275fa83871550ca
David Ulloa <dulloa6310@gmail.com>
2021-10-23 21:58:48 -0400

    Added Fetch Data to PiecePage

--- client/src/Components/PiecePage/PiecePage.jsx
+++ client/src/Components/PiecePage/PiecePage.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import { useParams } from "react-router-dom";
 
@@ -6,23 +6,37 @@ import Piece from "../Shared/Piece";
 
 import styles from "./PiecePage.module.css";
 
-const DUMMY_DATA = {
-  title: "Haiku",
-  author: "David Ulloa",
-  content: "An ocean voyage\nAs waves break over the bow\nthe sea welcomes me",
-};
-
 function PiecePage(props) {
   const id = useParams().pieceId;
+  const [piece, setPiece] = useState();
+  const [loading, setLoading] = useState(true);
 
-  function fetchData() {}
+  useEffect(() => {
+    fetch(`http://localhost:5000/api/v1/pieces/${id}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setPiece(data.data.piece);
+        return data;
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return (
+      <section className={styles["piece-section"]}>
+        <h1>Loading</h1>
+      </section>
+    );
+  }
 
   return (
     <section className={styles["piece-section"]}>
       <Piece
-        title={DUMMY_DATA.title}
-        author={DUMMY_DATA.author}
-        content={DUMMY_DATA.content}
+        title={piece.title}
+        author={piece.author}
+        content={piece.text}
         textStyle={{
           textAlign: "center",
           fontSize: "1.5em",
--- server/routes/pieces.js
+++ server/routes/pieces.js
@@ -19,6 +19,23 @@ router.get("/", async (req, res) => {
   }
 });
 
+router.get("/:pieceId", async (req, res) => {
+  try {
+    const piece = await Piece.findById(req.params.pieceId);
+    res.status(400).json({
+      status: "success",
+      data: {
+        piece,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: `${err}`,
+    });
+  }
+});
+
 router.post("/", async (req, res, next) => {
   try {
     const piece = await Piece.create(req.body);

4b58ab3b4399df15b09bb20774eff12f32184b78
David Ulloa <dulloa6310@gmail.com>
2021-11-03 17:03:43 -0400

    Edited dotenv to .config()

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
 
-require("dotenv/config");
+require("dotenv").config();
 
 const app = express();
 

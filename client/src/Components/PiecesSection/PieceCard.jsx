3d6dd5917a079b60b1510e8990f0194f955852cf
David Ulloa <dulloa6310@gmail.com>
2021-11-03 13:20:33 -0400

    Fixed  server and added hearts to PieceCard

--- client/src/Components/PiecesSection/PieceCard.jsx
+++ client/src/Components/PiecesSection/PieceCard.jsx
@@ -1,21 +1,29 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { FaHeart } from "react-icons/fa";
+
 import Card from "../Shared/UI/Card";
 
 import styles from "./PieceCard.module.css";
 
 function Piece(props) {
   return (
-    <Link to={`piece/${props.id}`} className={styles["link-style"]}>
-      <Card className={styles["piece"]}>
+    <Card className={styles["piece"]}>
+      <Link to={`piece/${props.id}`} className={styles["link-style"]}>
         <div>
           <h1 className={styles["title"]}>{props.title} </h1>
           <h2>By {props.author}</h2>
         </div>
         {/* {props.id} */}
         <h2>{props.type}</h2>
-      </Card>
-    </Link>
+      </Link>
+      <button className={styles["heart-button"]}>
+        <FaHeart
+          className={`${styles["heart-icon"]} ${styles["selected-icon"]}`}
+          size={30}
+        ></FaHeart>
+      </button>
+    </Card>
   );
 }
 
--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ require("dotenv/config");
 
 const app = express();
 
-app.use(bodyParser.json());
+app.use(express.json());
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
@@ -27,12 +27,16 @@ app.use((req, res, next) => {
 
 const piecesRoutes = require("./routes/pieces");
 
-// Connect to DB
-
 mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true }, () => {
   console.log("Connected");
 });
 
 app.use("/api/v1/pieces", piecesRoutes);
 
-app.listen(5000);
+app.use("/", (req, res) => {
+  res.json({
+    success: true,
+  });
+});
+
+app.listen(process.env.PORT || 5000);

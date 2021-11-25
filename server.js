f954b98d23687a12dbc0c54c39d0d7ed01a0f0d5
David Ulloa <dulloa6310@gmail.com>
2021-11-25 14:58:12 -0500

    Revert "Added Helmet, Cors, and refactored to App.js"
    This reverts commit 0034814317dc1c1708f729669e646ecca72b5e68.

--- server.js
+++ server.js
@@ -1,14 +1,37 @@
+const bodyParser = require("body-parser");
+const express = require("express");
 const mongoose = require("mongoose");
-
-const app = require("./app");
+const path = require("path");
 
 require("dotenv").config();
+
+const app = express();
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+
+  next();
+});
+
+const piecesRoutes = require("./routes/pieces-routes");
 
 mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true }, () => {
   console.log("Connected");
 });
-
-const piecesRoutes = require("./routes/pieces-routes");
 
 app.use("/api/v1/pieces", piecesRoutes);
 

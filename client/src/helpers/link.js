2e0df37d9bd07bb64aa7cf2d73e22bb4c11f6d90
David Ulloa <dulloa6310@gmail.com>
2021-12-14 13:39:36 -0500

    Update link.js

--- client/src/helpers/link.js
+++ client/src/helpers/link.js
@@ -1,5 +1,5 @@
 export function getLink() {
-  let apiURL = "https://yourpapyrs.com/api/v1";
+  let apiURL = "https://www.yourpapyrs.com/api/v1";
   // console.log(process.env.REACT_APP_PRODUCTION_URL);
   // if (process.env.REACT_APP_DEVELOPMENT) {
   //   apiURL = `${process.env.REACT_APP_LOCALHOST_URL}/api/v1`;

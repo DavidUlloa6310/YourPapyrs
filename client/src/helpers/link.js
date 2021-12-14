42245dcead299387a09a999002603b02b71c7be9
David Ulloa <dulloa6310@gmail.com>
2021-12-14 13:30:52 -0500

    Update link.js

--- client/src/helpers/link.js
+++ client/src/helpers/link.js
@@ -1,10 +1,12 @@
 export function getLink() {
   let apiURL;
+  console.log(process.env.REACT_APP_DEVELOPMENT);
   if (process.env.REACT_APP_DEVELOPMENT) {
     apiURL = `${process.env.REACT_APP_LOCALHOST_URL}/api/v1`;
   } else {
     apiURL = `${process.env.REACT_APP_PRODUCTION_URL}/api/v1`;
   }
 
+  console.log(apiURL);
   return apiURL;
 }

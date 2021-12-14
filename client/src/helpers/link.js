affdef5cc7d44654f51a6afc6e0f720e09b8f3c0
David Ulloa <dulloa6310@gmail.com>
2021-12-14 13:36:06 -0500

    Update link.js

--- client/src/helpers/link.js
+++ client/src/helpers/link.js
@@ -1,11 +1,11 @@
 export function getLink() {
-  let apiURL;
-  console.log(process.env.REACT_APP_DEVELOPMENT);
-  if (process.env.REACT_APP_DEVELOPMENT) {
-    apiURL = `${process.env.REACT_APP_LOCALHOST_URL}/api/v1`;
-  } else {
-    apiURL = `${process.env.REACT_APP_PRODUCTION_URL}/api/v1`;
-  }
+  let apiURL = "https://yourpapyrs.com/api/v1";
+  // console.log(process.env.REACT_APP_PRODUCTION_URL);
+  // if (process.env.REACT_APP_DEVELOPMENT) {
+  //   apiURL = `${process.env.REACT_APP_LOCALHOST_URL}/api/v1`;
+  // } else {
+  //   apiURL = `${process.env.REACT_APP_PRODUCTION_URL}/api/v1`;
+  // }
 
   console.log(apiURL);
   return apiURL;
